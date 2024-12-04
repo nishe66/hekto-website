e17f99d9076580e347f12a6bac1e658c3d1d9be8
nishe66 <sumiyajahan29@gmail.com>
2024-12-04 20:22:46 +0600

    five commit

--- src/components/Featuredproduct.jsx
+++ src/components/Featuredproduct.jsx
@@ -5,7 +5,7 @@ import { BsCart3 } from "react-icons/bs";
 import { FaRegHeart } from "react-icons/fa6";
 import { LuSearchX } from "react-icons/lu";
 
-const Featuredproducts = () => {
+const Featuredproduct = () => {
   let data = useContext(apiData);
   return (
     <section className='py-24'>
@@ -47,4 +47,4 @@ const Featuredproducts = () => {
   )
 }
 
-export default Featuredproducts
+export default Featuredproduct
